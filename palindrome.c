3e336d507da6c05c42ab8f07e4268769693786c3
Robert Viens <bobv@Roberts-MacBook-Air.local>
2020-05-27 09:01:18 +0200

    -a

---
+++ palindrome.c
@@ -0,0 +1,44 @@
+/* 
+Takes a word and checks if it's a palindrom or not
+First let's see if I can take a word and return the number of letters in it...
+*/
+
+#include<stdio.h>
+#include<string.h>
+#define MAX_STRING_LENGTH 25
+
+int main(void) {
+	char word[MAX_STRING_LENGTH];
+	char wordReverse[MAX_STRING_LENGTH];
+	int howManyLetters = 0;
+	int countUp = 0;
+	int isPalindrome;
+
+
+	printf("Enter a word: ");
+	scanf("%s", word);
+
+	// count letters
+	while(word[howManyLetters] != '\0') {
+		howManyLetters++;
+	}
+
+	// fill wordReverse
+	while(howManyLetters > 0) {
+		wordReverse[countUp] = word[howManyLetters];
+		howManyLetters--;
+		countUp++;
+	}
+
+	// compare words
+	isPalindrome = strcmp(word, wordReverse);
+
+	printf("\nword: %s\nwordReverse: %s\niPalondrome: %d\ncountUp: %d\nhowManyLetters: %d\n\n", 
+		word, wordReverse, isPalindrome, countUp, howManyLetters);
+
+	if (isPalindrome == 0) {
+		printf("Your word %s is a palindrome!\n", word);
+	}
+	else printf("Your word %s is not a palindrome.\n", word);
+}
+

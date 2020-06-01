c2d8905d5e2b1cc0912d5e4f5c8b9df0b0cfdba5
Robert Viens <bobv@Roberts-MacBook-Air.local>
2020-06-01 12:39:26 +0200

    removed flawed version code

--- palindrome.c
+++ palindrome.c
@@ -8,32 +8,21 @@ Takes a word and checks if it's a palindrome
 #define MAX_STRING_LENGTH 25
 
 int main(void) {
+	
+	// variables
 	char word[MAX_STRING_LENGTH];
-	char wordReverse[MAX_STRING_LENGTH];
 	int letterCount = 0;
 	int countUp = 0;
 	int isPalindrome;
 
 	// prompt user
-	printf("Enter a word: ");
+	printf("\nEnter a word: ");
 	scanf("%s", word);
 
 	// count letters
 	while(word[letterCount] != '\0') {
 		letterCount++;
 	}
-
-	// // fill wordReverse
-	// while(letterCount > 0) {
-	// 	wordReverse[countUp] = word[letterCount - 1];
-	// 	letterCount--;
-	// 	countUp++;
-	// }
-	// // add null terminator to wordReverse
-	// wordReverse[countUp] = '\0';
-
-	// // compare forwards and backwards
-	// isPalindrome = strcmp(word, wordReverse);
 
 	// Mike's method
 	while(letterCount - 1 >= countUp) {
@@ -48,14 +37,9 @@ int main(void) {
 		}
 	}
 
-	// sanity check
-	printf("\nword: %s\n\niPalondrome: %d\ncountUp: %d\nletterCount: %d\n\n", 
-		word, isPalindrome, countUp, letterCount);
-
 	// output result
 	if (isPalindrome == 0) {
-		printf("Result: %s is a palindrome!\n", word);
+		printf("Result: %s is a palindrome!\n\n", word);
 	}
-	else printf("Result: %s is not a palindrome.\n", word);
+	else printf("Result: %s is not a palindrome.\n\n", word);
 }
-
